4a2943394e5dc38bff212772ce53df7c8513424a
Rudolf Polzer <divverent@alientrap.org>
2010-05-08 18:27:59 +0200

    cdtracks management scripts fixed

--- cdtracks.sh
+++ cdtracks.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+IGNORE=thunder
+
 set -ex
 cd sound/cdtracks
 for F in *.ogg; do
 	T=${F%.ogg} # for tracker files we still need the ext
-	if ! grep "^cdtrack [1-9][0-9]* $T" ../../cdtracks.cfg >/dev/null; then
-		n=`tail -n 1 ../../cdtracks.cfg | cut -d ' ' -f 2`
-		n=$(($n+1))
-		echo "_cdtrack $n $T" >> ../../cdtracks.cfg
-	fi
+	case " $IGNORE " in
+		*" $T "*)
+			;;
+		*)
+			if ! grep "^_cdtrack [1-9][0-9]* $T" ../../cdtracks.cfg >/dev/null; then
+				n=`tail -n 1 ../../cdtracks.cfg | cut -d ' ' -f 2`
+				n=$(($n+1))
+				echo "_cdtrack $n $T" >> ../../cdtracks.cfg
+			fi
+	esac
 done
